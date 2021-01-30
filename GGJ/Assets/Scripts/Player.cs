cbb9ccbaf846dd1ce9da2c5c2a5fb04b0bffeca2
knoodl <knguye93@uwo.ca>
2021-01-30 14:35:00 -0500

    Added scripts for orbs & created prefab
    0 - Left, 1 - Right, 2 - Up

--- GGJ/Assets/Scripts/Player.cs
+++ GGJ/Assets/Scripts/Player.cs
@@ -9,10 +9,24 @@ public class Player : MonoBehaviour
     Vector3 velocity = Vector3.zero;
     float verticalSpeed = 0f;
 
+    // Ability/Orb related
+
+    public bool[] activeAbilities; // An array of 3 booleans
+    /*
+        True if enabled
+        0 - Left
+        1 - Right
+        2 - Up
+    */
+
+    public int lastAbilityHeld;
+
+    public GameObject orb; // to instantiate
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -30,5 +44,17 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         transform.position += velocity * Time.fixedDeltaTime * speed;
+    }
+
+    public void changeAbilities(int ability) {
+        // Enable the ability picked up, 
+        activeAbilities[ability] = true;
+
+        // .. and set the last obtained ability to false
+        activeAbilities[lastAbilityHeld] = false;
+
+        // Instantiate an orb @ the player's position with the ability last held
+        GameObject orbDropped = Instantiate(orb, transform.position, transform.rotation);
+        orbDropped.GetComponent<Orb>().powerUpEffect = lastAbilityHeld;
     }
 }
