e035b31dae9de1e849ed4e089982a92e41bfd536
Kevin Nguyen <kevin-nguyen@live.ca>
2021-01-30 14:59:12 -0500

    Added scripts for orbs & created prefab (#1)
    0 - Left, 1 - Right, 2 - Up
    
    Co-authored-by: knoodl <knguye93@uwo.ca>

---
+++ GGJ/Assets/Scripts/Orb.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+    public int powerUpEffect;
+    public Sprite[] sprites;
+
+    private void Start() {
+        // Set the appearance to fit the powerUp Effect
+        setAppearance();
+    }
+
+    void setAppearance(){
+        GetComponent<SpriteRenderer>().sprite = sprites[powerUpEffect];
+    }
+
+    void onTriggerEnter(Collider col){
+        // If collider is player tag
+        if (col.gameObject.name == "Player"){
+            col.gameObject.GetComponent<Player>().changeAbilities(powerUpEffect); // Give the player this ability
+            Destroy(this); // Delete the orb
+        }
+    }
+}
