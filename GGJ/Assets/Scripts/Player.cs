5584984d5f14e58553611c1bc3b12c1f7e137838
Emanuel Sharma <send.emanuel@hotmail.com>
2021-01-31 11:11:33 -0500

    Changes to scripts, art, and animations

--- GGJ/Assets/Scripts/Player.cs
+++ GGJ/Assets/Scripts/Player.cs
@@ -12,22 +12,37 @@ public class Player : MonoBehaviour
     [Header("Components")]
     public Rigidbody2D rb;
     public InputManager input;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
     void Update()
     {
         // Check grounded
         Vector2 crrtVelocity = rb.velocity;
+        animator.SetBool("Falling", crrtVelocity.y < 0f);
         mIsGrounded = crrtVelocity.y == 0;
 
         // Kinematics
         // Horizontal
         Vector2 newVelocity = crrtVelocity;
         newVelocity.x = input.horizontal * movementSpeed;
+        animator.SetBool("Running", newVelocity.x != 0f);
+        if (newVelocity.x != 0f)
+        {
+            spriteRenderer.flipX = newVelocity.x < 0f;
+        }
         // Vertical
         if (mIsGrounded && input.jump)
         {
-            newVelocity += Vector2.up * jumpSpeed;
+            animator.SetTrigger("Jumping");
         }
+        rb.velocity = newVelocity;
+    }
+
+    public void Jump()
+    {
+        Vector2 newVelocity = rb.velocity;
+        newVelocity += Vector2.up * jumpSpeed;
         rb.velocity = newVelocity;
     }
 
